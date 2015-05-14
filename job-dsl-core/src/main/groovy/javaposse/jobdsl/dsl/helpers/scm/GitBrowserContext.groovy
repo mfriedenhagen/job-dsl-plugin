ce0f18dc617bdfa3f2313e8b79b6f2947500e0a3
Mirko Friedenhagen <mfriedenhagen@gmail.com>
2015-05-14 22:12:28 +0200

    Implement more gitbrowsers.

--- job-dsl-core/src/main/groovy/javaposse/jobdsl/dsl/helpers/scm/GitBrowserContext.groovy
+++ job-dsl-core/src/main/groovy/javaposse/jobdsl/dsl/helpers/scm/GitBrowserContext.groovy
@@ -10,4 +10,23 @@ class GitBrowserContext implements Context {
             delegate.url(url)
         }
     }
+
+    void github(String url) {
+        browser = NodeBuilder.newInstance().browser(class: 'hudson.plugins.git.browser.GithubWeb') {
+            delegate.url(url)
+        }
+    }
+
+    void gitweb(String url) {
+        browser = NodeBuilder.newInstance().browser(class: 'hudson.plugins.git.browser.GitWeb') {
+            delegate.url(url)
+        }
+    }
+
+    void gitlab(String url, String version) {
+        browser = NodeBuilder.newInstance().browser(class: 'hudson.plugins.git.browser.GitLab') {
+            delegate.url(url)
+            delegate.version(version)
+        }
+    }
 }
